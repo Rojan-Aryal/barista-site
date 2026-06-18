8d1ef3261dd4c10b2155facd7d8069f9fa7f9082
Rojan-Aryal <rojan.aryal@arforgetech.com>
2026-06-18 14:06:23 +0545

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,13 @@
 import "./globals.css";
 
-export default function RootLayout({children}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body>
-        <header className="header">
-          <h1>Barista Training Center</h1>
-
-          <nav>
-            <a href="/">Home</a>
-            <a href="/about">About</a>
-            <a href="/courses">Courses</a>
-            <a href="/contact">Contact</a>
-          </nav>
-        </header>
-
-        {children}
-
-        <footer className="footer">
-          <p>Barista Training Center</p>
-        </footer>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
